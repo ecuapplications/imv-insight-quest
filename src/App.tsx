63d5abfcd4ecd03b4a9e94ee294493b91c0a3db9
Gustavo Amarista <zipnegocios@gmail.com>
2026-09-10 10:38:36 +0000

    feat: permitir configurar el basename de las rutas via VITE_BASE_PATH

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.VITE_BASE_PATH || "/"}>
         <Routes>
           <Route path="/" element={<Survey />} />
           <Route path="/s/:codigo" element={<Survey />} />
